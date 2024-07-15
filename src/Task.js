fdcc4cbc8b44f1e6fc012679f3eeb2b81572e003
Louay Lahfaoui <louay.lahfaoui@telecom-paris.fr>
2024-07-15 10:26:49 +0200

    first changes

---
+++ src/Task.js
@@ -0,0 +1,11 @@
+export const Task = (props) =>{
+    return (
+        <div className='task'
+            style={{backgroundColor : props.completed ? "green":"white"}}
+        >
+            <h1 >{props.taskName}</h1>
+            <button onClick={() =>props.deleteTask(props.id)}>Done</button>
+            <button onClick ={()=> props.completeTask(props.id)}>Complete</button>
+          </div>
+    )
+}
